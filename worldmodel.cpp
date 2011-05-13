52e42fd013c78f174d5a6e1ab94057f04394ccbc
Christoph Schwering <schwering@gmail.com>
2011-05-13 18:45:25 +0200

    Started listener for worldmodel information.

--- worldmodel.cpp
+++ worldmodel.cpp
@@ -19,17 +19,28 @@ void cWorldModel::handle(cDriver& state)
 
 void cWorldModel::process(double time, const tCarElt* car)
 {
-  const char* name = car->_name;
-  const float veloc = car->_speed_x;
-  const float accel = car->_accel_x;
+  CarInfo ci;
+  ci.name = car->_name;
+  ci.veloc = car->_speed_x;
+  ci.accel = car->_accel_x;
   const tTrkLocPos trkPos = car->_trkPos;
-  float yaw = car->_yaw - RtTrackSideTgAngleL(const_cast<tTrkLocPos*>(&trkPos));
-  NORM_PI_PI(yaw);
+  ci.yaw = car->_yaw - RtTrackSideTgAngleL(const_cast<tTrkLocPos*>(&trkPos));
+  NORM_PI_PI(ci.yaw);
   const tTrackSeg* seg = trkPos.seg;
-  const float dist = seg->lgfromstart + trkPos.toStart;
+  ci.pos = seg->lgfromstart + trkPos.toStart;
+  ci.offset = -1.0f * trkPos.toMiddle;
+#if 0
   printf("%lf: %7s v = %5.2fm/s = %5.1fkm/h f = %6.2fm/s^2 "\
          "yaw = %5.2frad = %5.1fdeg dist=%.0fm\n",
          time, name, veloc, mps2kmph(veloc), accel,
-         yaw, rad2deg(yaw), dist);
+         yaw, rad2deg(yaw), pos);
+#else
+  printf("(veloc('%s') = %f & yaw('%s') = %f, %lf)",
+         ci.name, ci.veloc, ci.name, ci.yaw, ci.time);
+#endif
+}
+
+void cWorldModel::addListener(cWorldModel::cListener* listener)
+{
 }
 
--- worldmodel.h
+++ worldmodel.h
@@ -6,8 +6,27 @@
 class cWorldModel : public cDriver::cHandler
 {
  public:
+  struct CarInfo
+  {
+    const char* name;
+    double time;
+    float veloc;
+    float accel;
+    float yaw;
+    float pos;
+    float offset;
+  };
+
+  class cListener
+  {
+   public:
+    virtual void processCarInfo(const CarInfo& carInfo);
+  };
+
   virtual int priority() const;
   virtual void handle(cDriver& state);
+
+  void addListener(cListener* listener);
 
  private:
   void process(double time, const tCarElt* car);
