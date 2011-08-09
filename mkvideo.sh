07ecb416e995710b744e29f91df175d218059d2f
Christoph Schwering <schwering@gmail.com>
2011-08-10 01:46:07 +0200

    Skript to create video out of images.

---
+++ mkvideo.sh
@@ -0,0 +1,17 @@
+dir=${HOME}/Desktop/torcs_captures
+
+for f in $(ls ${dir}/*.pnm)
+do
+        g=$(echo $f | sed -e 's/\.pnm/\.png/g')
+        echo "Converting $f -> $g."
+        if [ ! -f "$g" ]
+        then
+                pnmtopng $f >$g || exit
+        fi
+done
+
+echo "Creating video."
+mencoder mf://${dir}/*.png -mf w=800:h=600:fps=25:type=png -ovc lavc -lavcopts vcodec=mpeg4 -oac copy -o ${dir}/output.avi || exit
+
+mplayer ${dir}/output.avi || exit
+
