cd9c7259ea9a4b40aafd9c7c88f899778408cffd
Christoph Schwering <schwering@gmail.com>
2011-10-17 14:42:26 +0200

    Improved run.sh to work local, too.

--- run.sh
+++ run.sh
@@ -1,4 +1,43 @@
-#../../../bin/torcs | ssh rambo "./ctrl -d Documents/Prolog/ccgolog -l -p 8 -t 3 -v -s -f /home/cschwering/sub >out"
+#!/bin/bash
 
-ssh rambo "rm -f sub && touch sub && tail -f sub" | ../../../bin/torcs | ssh rambo "./ctrl -d Documents/Prolog/ccgolog -l -p 8 -t 3 -v -s -f /home/cschwering/sub >out"
+REMOTE=0
+
+if [ "${REMOTE}" == "1" ]
+then
+        HOST="rambo"
+        PRHOME=/home/cschwering 
+        CPUS=8
+else
+        HOST="$(hostname)"
+        PRHOME=${HOME}
+        CPUS=1
+fi
+PRGOLOG=${PRHOME}/Documents/Prolog/ccgolog
+
+function pr_exec
+{
+        CMD="$@"
+        if [ "${REMOTE}" == "1" ]
+        then
+                ssh rambo "$CMD" || exit
+        else
+                $CMD || exit
+        fi
+}
+
+function pr_exec_log
+{
+        CMD="$@"
+        if [ "${REMOTE}" == "1" ]
+        then
+                ssh rambo "$CMD > \"${PRHOME}/out\"" || exit
+        else
+                $CMD > "${PRHOME}/out" || exit
+        fi
+}
+
+echo "TORCS + PlanRecog @ ${HOST}" &&\
+pr_exec rm -f ${PRHOME}/sub && touch ${PRHOME}/sub && tail -f ${PRHOME}/sub |\
+../../../bin/torcs |\
+pr_exec_log ${PRGOLOG}/ctrl -d ${PRGOLOG} -l -p ${CPUS} -t 3 -v -s -f ${PRHOME}/sub
 
