921a09fccb2b99953893ba91ab8afd3274d5ef21
Christoph Schwering <schwering@gmail.com>
2011-10-23 22:35:55 +0200

    Fixed VNC problems in run.sh.

--- run.sh
+++ run.sh
@@ -2,25 +2,39 @@
 
 REMOTE=1
 
-if [ "$1" == "-l" -o "$1" == "--local" ]
-then
-        REMOTE=0
-elif [ "$1" == "-r" -o "$1" == "--remote" ]
-then
-        REMOTE=1
-fi
+function vpn_connect
+{
+        if [ "${HOST}" == "rambo" -a "$(ifconfig | grep 'inet addr' | grep '13[47]')" == "" ]
+        then
+                echo "Connecting to VPN"
+                sudo vpnc-connect || exit
+        fi
+}
 
-if [ "${REMOTE}" == "1" ]
-then
-        HOST="rambo"
-        PRHOME=$(ssh rambo 'echo ${HOME}')
-        CPUS=8
-else
-        HOST="$(hostname)"
-        PRHOME=${HOME}
-        CPUS=1
-fi
-PRGOLOG=${PRHOME}/Documents/Prolog/ccgolog
+function init_vars
+{
+        if [ "$1" == "-l" -o "$1" == "--local" ]
+        then
+                REMOTE=0
+        elif [ "$1" == "-r" -o "$1" == "--remote" ]
+        then
+                REMOTE=1
+        fi
+
+        if [ "${REMOTE}" == "1" ]
+        then
+                HOST="rambo"
+                vpn_connect || exit
+                PRHOME=$(ssh rambo 'echo ${HOME}')
+                CPUS=8
+        else
+                HOST="$(hostname)"
+                PRHOME=${HOME}
+                CPUS=1
+        fi
+
+        PRGOLOG=${PRHOME}/Documents/Prolog/ccgolog
+}
 
 function pr_exec
 {
@@ -33,10 +47,14 @@ function pr_exec
         fi
 }
 
-echo "TORCS + PlanRecog" &&\
-echo "HOST   = ${HOST}" &&\
-echo "PRHOME = ${PRHOME}" &&\
-echo "CPUS   = ${CPUS}" &&\
+echo "TORCS + PlanRecog"
+
+init_vars || exit
+
+echo "HOST   = ${HOST}"
+echo "PRHOME = ${PRHOME}"
+echo "CPUS   = ${CPUS}"
+
 pr_exec rm -f ${PRHOME}/sub \&\& touch "${PRHOME}/sub" \&\& tail -f "${PRHOME}/sub" |\
 ../../../bin/torcs |\
 pr_exec "${PRGOLOG}/ctrl" -d "${PRGOLOG}" -l -p ${CPUS} -t 3 -v -s -f "${PRHOME}/sub" \> "${PRHOME}/out"
