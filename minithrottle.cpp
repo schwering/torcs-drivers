71db158ab61c7e7210d1702d64aecd07ae27061f
Christoph Schwering <schwering@kbsg.rwth-aachen.de>
2012-12-06 19:17:42 +0100

    Improved throttle handler.
    Thus, robots are better at keeping a specific speed.

--- minithrottle.cpp
+++ minithrottle.cpp
@@ -21,7 +21,13 @@ void cMiniThrottle::handle(cDriver& state)
 
   const float speedKmph = mps2kmph(car->_speed_x);
   const float diff = maxKmph - speedKmph;
-  car->_accelCmd = restrictRange(0.0f, 1.0f, diff / 30.0);
-  car->_brakeCmd = restrictRange(0.0f, 1.0f, -1.0f * diff / 30.0);
+  // I found the following constant values by trial and error (for the
+  // blue cobra car), but there is some intuition behind them:
+  const double BROADENING = 10.0; // stretch diff^3 function somewhat
+  const double ACCELBOOST = 13.5; // accel has much less effect than brake
+  const double NOACCEL = 0.15; // before braking, we can just avoid accel
+  const double cmd = diff*diff*diff * BROADENING;
+  car->_accelCmd = (float) restrictRange(0.0, 1.0, ACCELBOOST * cmd);
+  car->_brakeCmd = -1.0f * (float) restrictRange(-1.0, 0.0, cmd + NOACCEL);
 }
 
